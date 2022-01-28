9336b641d9995ac136a213ab85237535394b4953
Niko <nikob381@gmail.com>
2022-01-28 16:00:19 -0500

    Added temp code for altering element attributes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import EmailPreview from './components/EmailPreview';
 import RawHtml from './components/RawHtml';
 
 const App = () => {
-  const [emailBody, setEmailBody] = useState('');
-  const [htmlEmail, setHtmlEmail] = useState('<strong>Hello</strong>');
+  const [htmlEmail, setHtmlEmail] = useState('');
+
+  // Add specific attributes to certain tags if we need to later
+  // const [modifiedHtml, setModifiedHtml] = useState('');
+  // useEffect(() => {
+  //   setModifiedHtml(modifyHtml(htmlEmail));
+  // }, [htmlEmail]);
+
+  // const modifyHtml = (html) => {
+  //   return html.replaceAll(
+  //     '<p>',
+  //     '<p style="margin: 0; font-size: 16px; text-align: justify; mso-line-height-alt: 24px;">'
+  //   );
+  // };
 
   return (
     <div className="App container mx-auto mt-4">
       <h1 className="text-center text-3xl py-4">ENB Builder</h1>
       <div className="mx-auto w-full h-min-screen grid grid-cols-2 gap-4">
-        <EmailEditor />
+        <EmailEditor htmlEmail={htmlEmail} setHtmlEmail={setHtmlEmail} />
         <EmailPreview htmlEmail={htmlEmail} />
         <RawHtml htmlEmail={htmlEmail} />
       </div>
