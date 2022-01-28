69d8fa62635651ef5e76df8a53b0e6610283e657
Niko <nikob381@gmail.com>
2022-01-28 11:48:18 -0500

    Basic page layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [emailBody, setEmailBody] = useState('');
+  const [htmlEmail, setHtmlEmail] = useState('<strong>Hello</strong>');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App container mx-auto mt-4">
+      <h1 className="text-center text-3xl py-4">ENB Builder</h1>
+      <div className="mx-auto w-full h-min-screen grid grid-cols-2 gap-4">
+        <div
+          className="container mx-auto min-h-full h-96"
+          id="editor-container"
         >
-          Learn React
-        </a>
-      </header>
+          <h2>Editor</h2>
+          <textarea
+            id="editor"
+            className="font-mono w-full h-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            placeholder="Enter the body of your lead article here."
+            onChange={''}
+            // value={emailBody}
+          />
+        </div>
+        <div className="container mx-auto h-2/6" id="preview-container">
+          <h2>Preview</h2>
+          <div
+            id="preview"
+            className="prose w-full h-96 px-3 py-2 text-gray-700 border rounded-lg focus:outline-none overflow-scroll"
+            rows="6"
+            dangerouslySetInnerHTML={{ __html: htmlEmail }}
+          ></div>
+        </div>
+        <div className="container mx-auto col-span-2" id="raw-html">
+          <h2>Raw HTML</h2>
+          <textarea
+            id="raw-html-textarea"
+            className="font-mono w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            value={htmlEmail}
+          />
+        </div>
+      </div>
     </div>
   );
 };
