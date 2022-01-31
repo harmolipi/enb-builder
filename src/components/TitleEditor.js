47428f19554ed5b02558cdd16a985cf00acb8e50
Niko <nikob381@gmail.com>
2022-01-31 15:50:09 -0500

    Removed unnecessary attribute

--- src/components/TitleEditor.js
+++ src/components/TitleEditor.js
@@ -5,10 +5,9 @@ const TitleEditor = ({ title, onChange }) => {
       <input
         className="w-full h-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
         id="title-editor"
-        theme="snow"
         value={title}
         onChange={(e) => onChange(e.target.value)}
-        placeholder="Enter the title of your lead article here."
+        placeholder="Enter the title here."
       ></input>
     </div>
   );
